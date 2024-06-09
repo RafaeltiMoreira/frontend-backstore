a058b5bb5c595f4592b8650e3dab1fd5aca4a8d3
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-06-09 14:28:45 -0300

    Implementando responsividade para `mobile`

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,31 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
 import './Header.css'
 
 export default function Header() {
-    return (
-        <header>
-          <img src={logo} alt="Imagem logo Salvatore Academy" className='logo' />
+  const [menuOpen, setMenuOpen] = useState(false);
 
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/create">Create</Link>
-            <Link to="/update">Update</Link>
-            <Link to="/delete">Delete</Link>
-          </nav>
-        </header>
-    )
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header>
+      <img src={logo} alt="Imagem logo Salvatore Academy" className='logo' />
+
+      <nav className={menuOpen ? 'open' : ''}>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create</Link>
+        <Link to="/update">Update</Link>
+        <Link to="/delete">Delete</Link>
+      </nav>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+    </header>
+  )
 }
